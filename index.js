7d2d6be36291fa0575c06ee0bef7c2b3b2e5cabc
witty-abstraction-1855 <merve.kastan4@gmail.com>
2020-10-29 21:46:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "hello" + specialGuest + "!";
 
-var topic = "Let's talk about space";
+var topic = "space";
 
-var conversation = `${"Let's talk about space"}`;
+var conversation = `${"Let's talk about"}`;
